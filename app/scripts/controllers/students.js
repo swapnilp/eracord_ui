8d8d95896e34dffb1e032eab737e63c88b627042
SwapnilPatil <swapnil.patil04@gmail.com>
2015-11-30 23:11:26 +0530

    work on student edit

--- app/scripts/controllers/students.js
+++ app/scripts/controllers/students.js
@@ -22,15 +22,20 @@ angular.module('eracordUiApp.controller')
       });
     };
 
-    if($location.path() === '/students/new'){
+    if($location.path() === '/students/new' || $location.path() === "/classes/" + $routeParams.class_id + "/students/new"){
       var students = Restangular.all("students");
       $scope.initl = 'Mr';
       $scope.gender = 'Male';
       $scope.optionalSubjects = [];
+      $scope.classStudents = !_.isUndefined($routeParams.class_id);
+      $scope.vm = {};
 
-      students.customGET("new").then(function(data){
+      students.customGET("new", {class_id: $routeParams.class_id}).then(function(data){
 	$scope.standards = data.standards;
 	$scope.batches = data.batches;
+	if($scope.classStudents){
+	  $scope.oSubjects= data.subjects;
+	}
       });
       
       $scope.selectOptionalSubject = function(){
@@ -41,6 +46,11 @@ angular.module('eracordUiApp.controller')
       
       $scope.registerStudent = function(){
 	$scope.vm.user.o_subjects = $scope.optionalSubjects;
+	$scope.vm.user.initl = $scope.initl;
+	$scope.vm.user.gender = $scope.gender;
+	if($scope.classStudents){
+	  $scope.vm.user.standard_id = $scope.standards[0].id;
+	}
 	students.post($scope.vm.user).then(function(data) {
 	  if(data.success) {
 	    $location.path("/students");
@@ -48,12 +58,60 @@ angular.module('eracordUiApp.controller')
 	    Flash.create('warning', data.message, 'alert-danger');
 	  }
 	})
+      }  
+    };
+    // end of new path
+
+    if($location.path() === "/students/" + $routeParams.student_id + "/show"){
+      var student = Restangular.one("students", $routeParams.student_id);
+      student.get().then(function(data){
+	if(data.success) {
+	  $scope.student = data.body.student;
+	}
+      });
+    };
+    // end of show path
+    
+    if($location.path() === "/students/" + $routeParams.student_id + "/edit"){
+      $scope.student_id = $routeParams.student_id;
+      var student = Restangular.one("students", $routeParams.student_id);
+      $scope.vm = {};
+
+      $scope.selectOptionalSubject = function(){
+	Restangular.one("standards", $scope.vm.user.standard_id).customGET("optional_subjects").then(function(data){
+	  $scope.oSubjects= data.subjects;
+	})
       }
       
-    }
-    
-    
-    
+      student.customGET('edit').then(function(data){
+	if(data.success) {
+	  
+	  $scope.batches = data.batches;
+	  $scope.standards = data.standards;
+	  $scope.oSubjects= data.subjects;
+	  $scope.vm.user = data.student;
+	  $scope.initl = data.student.initl;
+	  $scope.gender = data.student.gender;
+	  $scope.optionalSubjects = data.o_subjects;
+	  console.log($scope.vm.user);
+	  //$scope.optionalSubjects = data.student.
+	}
+      });
+
+      $scope.registerStudent = function() {
+	$scope.vm.o_subjects = $scope.optionalSubjects;
+	$scope.vm.user.initl = $scope.initl;
+	$scope.vm.user.gender = $scope.gender;
+
+	student.customPOST({student: $scope.vm.user, o_subjects: $scope.vm.o_subjects}, "update", {}).then(function(data){
+	  if(data.success) {
+	    $location.path("/students/"+$scope.student_id+"/show");
+	  }else {
+	  }
+	});
+      }
+    };
+    //end of edit path
     
   }]);
 
--- app/scripts/routes/students.js
+++ app/scripts/routes/students.js
@@ -15,8 +15,18 @@ app.config(function($routeProvider) {
       controller: 'StudentsCtrl',
       controllerAs: 'students'
     })
+    .when('/classes/:class_id/students/new', {
+      templateUrl: 'views/students/new.html',
+      controller: 'StudentsCtrl',
+      controllerAs: 'students'
+    })
     .when('/students/:student_id/show', {
       templateUrl: 'views/students/show.html',
+      controller: 'StudentsCtrl',
+      controllerAs: 'students'
+    })
+    .when('/students/:student_id/edit', {
+      templateUrl: 'views/students/new.html',
       controller: 'StudentsCtrl',
       controllerAs: 'students'
     });
