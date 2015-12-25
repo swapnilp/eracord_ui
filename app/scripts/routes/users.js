986a111b0db9fe201d9521129242e361410600db
SwapnilPatil <swapnil.patil04@gmail.com>
2015-12-25 22:31:12 +0530

    worked on user role base

---
+++ app/scripts/routes/users.js
@@ -0,0 +1,14 @@
+'use strict';
+var app;
+
+app = angular.module('eracordUiApp.routes');
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/user/change_password', {
+      templateUrl: 'views/users/edit.html',
+      controller: 'UserCtrl',
+      controllerAs: 'users'
+    })
+});
+
