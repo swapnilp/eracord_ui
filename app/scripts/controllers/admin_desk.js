8ef53b08e1d92304397ce2580218691a9f9ff942
SwapnilPatil <swapnil.patil04@gmail.com>
2016-01-20 09:57:25 +0530

    worked on calendar

--- app/scripts/controllers/admin_desk.js
+++ app/scripts/controllers/admin_desk.js
@@ -16,10 +16,8 @@ angular.module('eracordUiApp.controller')
     }
 
     $scope.events= [];
-    $scope.calenderExamSelect = true;
-    $scope.calenderCatlogSelect = false;
+    $scope.selectedCalenderType = 'exams';
     $scope.loadCalenderEvent = false;
-    $scope.loadCalenderTimeTableEvent = false;
 
     /* add custom event*/
     var addExamEvent = function(event) {
@@ -41,22 +39,32 @@ angular.module('eracordUiApp.controller')
       });
     };
 
-    $scope.load_desk_classes = function(){
+    var addOffClassEvent = function(type, title, date) {
+      $scope.events.push({
+	type: type,
+        title: title,
+	start: new Date(date),
+	allDay: true,
+	className: ['bg-red-danger']
+      });
+    };
+
+    var load_desk_classes = function(){
       var jkci_classes = Restangular.all("jkci_classes");
       jkci_classes.getList().then(function(data){
 	$scope.jkci_classes = data;
       });
     };
 
-    $scope.load_unassigned_classes = function(){
+    var load_unassigned_classes = function(){
       var unassigned_jkci_classes = Restangular.all("get_unassigned_classes");
       unassigned_jkci_classes.getList().then(function(data){
 	$scope.us_jkci_classes = data;
       });
     };
 
-    $scope.load_calender_exams = function(start, end) {
-      var exams = Restangular.all("exams").customGET("calender_index", {start: start, end: end}).then(function(data) {
+    var load_calender_exams = function(start, end) {
+      Restangular.all("exams").customGET("calender_index", {start: start, end: end}).then(function(data) {
 	if(data.success) {
 	  _.each(data.exams, function(exam){
 	    addExamEvent(exam);
@@ -66,9 +74,8 @@ angular.module('eracordUiApp.controller')
       });
     };
 
-    $scope.load_calender_time_table = function(start, end) {
-
-      var time_table = Restangular.all("time_tables").customGET("calender_index").then(function(data) {
+    var load_calender_time_table = function(start, end) {
+      Restangular.all("time_tables").customGET("calender_index").then(function(data) {
 	if(data.success) {
 	  var time_table_slot = null;
 	  for(var i=1; i< 8;i += 1) {
@@ -77,88 +84,52 @@ angular.module('eracordUiApp.controller')
 	      addTimeTableEvent('time_table', _.pluck(time_table_slot, 'name').join(','), [i]);
 	    }
 	  }
-	  $scope.loadCalenderTimeTableEvent = false;
+	  $scope.loadCalenderEvent = false;
 	}
       });
     };
     
-    $scope.calendarView = 'month';
-    $scope.calendarDate = new Date();
-
-    $scope.load_desk_classes();
-    $scope.load_unassigned_classes();
+     var load_calender_off_class = function(start, end) {
+      Restangular.all("off_classes").customGET("calender_index").then(function(data) {
+	if(data.success) {
+	  _.each(data.off_classes, function(off_class){
+	    addOffClassEvent('off class', off_class.name, off_class.date);
+	  });
+	  $scope.loadCalenderEvent = false;
+	}
+      });
+    };
+    
+    load_desk_classes();
+    load_unassigned_classes();
 
     //@@@@@@@@@@@@@@
 
     
-    $scope.reloadCalenderEvent = function() {
+    $scope.reloadCalenderEvent = function(selectedType) {
+      $scope.selectedCalenderType = selectedType;
       uiCalendarConfig.calendars['myCalendar'].fullCalendar('refetchEvents');
     };
 
-    $scope.examEvents = function( start, end, timezone, callback) {
+    $scope.loadEvents = function(start, end, timezone, callback) {
       $scope.loadCalenderEvent = true;
-      if($scope.calenderExamSelect) {
-	$scope.load_calender_exams(start.format("DD/MM/YYYY"), end.format("DD/MM/YYYY"));
-      } else {
-	$scope.loadCalenderEvent = false;
+      //$scope.events =[{__id: 1}];
+      if($scope.selectedCalenderType === 'exams') {
+	load_calender_exams(start.format("DD/MM/YYYY"), end.format("DD/MM/YYYY"));
+      }else if ($scope.selectedCalenderType === 'time_table'){
+	load_calender_time_table(start, end);
+      }else if($scope.selectedCalenderType === 'off_class'){
+	load_calender_off_class(start, end);
       }
+      console.log($scope.events);
     };
-
-    $scope.timeTableEvents = function(start, end, timezone, callback) {
-      $scope.loadCalenderTimeTableEvent = true;
-      if($scope.calenderCatlogSelect) {
-	$scope.load_calender_time_table(start, end);
-	//$scope.load_calender_exams(start.format("DD/MM/YYYY"), end.format("DD/MM/YYYY"));
-      } else {
-	$scope.loadCalenderTimeTableEvent = false;
-      }
-    }
     
-    /* event source that calls a function on every view switch */
-    $scope.eventsF = function (start, end, timezone, callback) {
-      var s = new Date(start).getTime() / 1000;
-      var e = new Date(end).getTime() / 1000;
-      var m = new Date(start).getMonth();
-      var events = [{title: 'Feed Me ' + m,start: s + (50000),end: s + (100000),allDay: false, className: ['customFeed']}];
-      callback(events);
-    };
-
-    /* alert on eventClick */
-    $scope.alertOnEventClick = function( date, jsEvent, view){
-      $scope.alertMessage = (date.title + ' was clicked ');
-    };
 
     $scope.alertOnDayClick = function( data){
       console.log(data);
     }
-    /* alert on Drop */
-     $scope.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view){
-       $scope.alertMessage = ('Event Droped to make dayDelta ' + delta);
-    };
-    /* alert on Resize */
-    $scope.alertOnResize = function(event, delta, revertFunc, jsEvent, ui, view ){
-       $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
-    };
     
     
-    /* add and removes an event source of choice */
-    $scope.addRemoveEventSource = function(sources,source) {
-      var canAdd = 0;
-      angular.forEach(sources,function(value, key){
-        if(sources[key] === source){
-          sources.splice(key,1);
-          canAdd = 1;
-        }
-      });
-      if(canAdd === 0){
-        sources.push(source);
-      }
-    };
-    
-    /* remove event */
-    $scope.remove = function(index) {
-      $scope.events.splice(index,1);
-    };
     /* Change View */
     $scope.changeView = function(view,calendar) {
       uiCalendarConfig.calendars[calendar].fullCalendar('changeView',view);
@@ -169,7 +140,7 @@ angular.module('eracordUiApp.controller')
         uiCalendarConfig.calendars[calendar].fullCalendar('render');
       }
     };
-    
+
      /* Render Tooltip */
     $scope.eventRender = function( event, element, view ) { 
         element.attr({'uib-tooltip': event.title,
@@ -188,16 +159,13 @@ angular.module('eracordUiApp.controller')
           center: 'title',
           right: 'today prev,next'
         },
-        eventClick: $scope.alertOnEventClick,
 	dayClick: $scope.alertOnDayClick,
-        eventDrop: $scope.alertOnDrop,
-        eventResize: $scope.alertOnResize,
         eventRender: $scope.eventRender
       }
     };
 
     /* event sources array*/
-    $scope.eventSources = [$scope.events, $scope.examEvents, $scope.timeTableEvents];
+    $scope.eventSources = [$scope.events, $scope.loadEvents]
     
     
   }]);
