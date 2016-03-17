72c7cfcb5321f24578862397aec50fb384da735d
SwapnilPatil <swapnil.patil04@gmail.com>
2016-03-17 19:31:41 +0530

    work on search

---
+++ app/scripts/directives/globel.js
@@ -0,0 +1,22 @@
+'use strict';
+var app;
+
+app = angular.module('eracordUiApp.directives');
+
+app.directive('dlKeyCode', function() {
+  return {
+    restrict: 'AE',
+    transclude: true,
+    link: function($scope, $element, $attrs) {
+      $element.bind("keypress", function(event) {
+        var keyCode = event.which || event.keyCode;
+        if (keyCode == $attrs.code) {
+          $scope.$apply(function() {
+            $scope.$eval($attrs.dlKeyCode, {$event: event});
+          });
+	  
+        }
+      });
+    }
+  };
+});
