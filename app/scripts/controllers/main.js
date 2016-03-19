5f179bc04bce127956f05df83d40e0e7ca4af0f7
SwapnilPatil <swapnil.patil04@gmail.com>
2016-03-19 13:40:53 +0530

    work on UI

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,14 +22,14 @@ angular.module('eracordUiApp.controller')
       return true;
     }
     
-    $scope.selectedChartType= 'daily';
-    $scope.exam_labels = $scope.labels = [];
-    $scope.exam_series = $scope.series = [];
-    $scope.exam_data = $scope.data = [];
-    $scope.exam_sum_data = $scope.sum_data = [];
+    $scope.selectedChartType = 'day';
+    $scope.off_class_labels = $scope.exam_labels = $scope.labels = [];
+    $scope.off_class_series = $scope.exam_series = $scope.series = [];
+    $scope.off_class_data = $scope.exam_data = $scope.data = [];
+    $scope.off_class_sum_data = $scope.exam_sum_data = $scope.sum_data = [];
     
-    var loadData = function(){
-      Restangular.all("organisations").customGET('absenty_graph_report').then(function(data){
+    var loadData = function(durationType){
+      Restangular.all("organisations").customGET('absenty_graph_report', {duration_type: durationType}).then(function(data){
 	if(data.success) {
 	  $scope.labels = data.headers;
 	  $scope.series = $scope.series = data.labels;
@@ -37,21 +37,34 @@ angular.module('eracordUiApp.controller')
 	  $scope.sum_data = data.sum_data;
 	}
       });  
-      Restangular.all("organisations").customGET('exams_graph_report').then(function(data){
+      Restangular.all("organisations").customGET('exams_graph_report', {duration_type: durationType}).then(function(data){
 	if(data.success) {
 	  $scope.exam_labels = data.headers;
 	  $scope.exam_series = $scope.series = data.labels;
 	  $scope.exam_data = data.data;
 	  $scope.exam_sum_data = data.sum_data;
 	}
-      });  
+      });
+      
+      Restangular.all("organisations").customGET('off_class_graph_report', {duration_type: durationType}).then(function(data){
+	if(data.success) {
+	  $scope.off_class_labels = data.headers;
+	  $scope.off_class_series = $scope.series = data.labels;
+	  $scope.off_class_data = data.data;
+	  $scope.off_class_sum_data = data.sum_data;
+	}
+      });
     };
 
-    loadData();
+    loadData('day');
+
+    $scope.reloadReport = function(chartType) {
+      loadData(chartType);
+    };
     
     
     $scope.onClick = function (points, evt) {
-      //console.log(points, evt);
+      console.log(points, evt);
     };
     
     this.awesomeThings = [
