3f9a1e934fcd5e7412de96169480a3eb522aa125
SwapnilPatil <swapnil.patil04@gmail.com>
2016-03-29 22:35:55 +0530

    worked on info

--- app/scripts/directives/organisations.js
+++ app/scripts/directives/organisations.js
@@ -262,7 +262,7 @@ app.directive('organisationStandards', function(Restangular) {
       hostUrl: '@'
     },
     templateUrl: 'views/organisations/organisation_standards.html',
-    controller: ['$scope', 'Restangular', 'Flash', '$location', '$cookieStore', function(scope, Restangular, Flash, $location, $cookieStore){
+    controller: ['$scope', 'Restangular', 'Flash', '$location', '$cookieStore', '$window', function(scope, Restangular, Flash, $location, $cookieStore, $window){
       scope.classes = [];
       scope.organisation_id = $cookieStore.get('currentUser').organisation_id;
       scope.requestLoading = true;
@@ -278,13 +278,15 @@ app.directive('organisationStandards', function(Restangular) {
       };
 
       scope.removeOrganisaitonStandard = function(standard, org_id) {
-	Restangular.all("/organisations").customPOST({standard_id: standard.id, organisation_id: org_id}, 'remove_standard_from_organisation', {}).then(function(data){
-	  if(data.success) { 
-	    standard.organisaitons = _.reject(standard.organisaitons, function(obj){return obj.organisation_id == org_id});
-	  }
-	})	
+	if($window.confirm('Are you sure?')){
+	  Restangular.all("/organisations").customPOST({standard_id: standard.id, organisation_id: org_id}, 'remove_standard_from_organisation', {}).then(function(data){
+	    if(data.success) { 
+	      standard.organisaitons = _.reject(standard.organisaitons, function(obj){return obj.organisation_id == org_id});
+	    }
+	  })
+	}
       }
-
+      
       scope.$watch('organisationStandardTab', function(){
 	if(scope.organisationStandardTab === 'true'){
 	  scope.loadOrganisationStandards();
