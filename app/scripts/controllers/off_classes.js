aafe2f16032320622218b701951eecf0b93337fb
SwapnilPatil <swapnil.patil04@gmail.com>
2016-08-02 13:04:43 +0530

    work on off classes

--- app/scripts/controllers/off_classes.js
+++ app/scripts/controllers/off_classes.js
@@ -27,15 +27,56 @@ angular.module('eracordUiApp.controller')
     if($location.path() === '/off_classes') {
 
       $scope.off_classes = [];
+      $scope.teachers = [];
+      $scope.classes = [];
+      $scope.filterOffClass = {};
+      $scope.requestLoading = true;
+      $scope.pagination = {current: 1};
+      $scope.totalOffClasses = 0;
+      $scope.showResetFilter = false;
+      
       var base_off_classes = Restangular.all("off_classes");
       
-      var load_off_classes = function() {
-	base_off_classes.customGET("").then(function(data) {
-	  $scope.off_classes = data.off_classes;
-	})
+      var get_filter_data = function(){
+	base_off_classes.customGET("get_filter_data").then(function(data) {
+	  if(data.success) {
+	    $scope.teachers = data.teachers;
+	    $scope.classes = data.standards;
+	  }
+	});
+      }
+      var getResultsPage = function(pageNumber, checkFilter) {
+	if(!checkFilter && _.size($scope.filterOffClass) === 0) {
+	  return true;
+	}
+	if(_.size($scope.filterOffClass) >  0) {
+	  $scope.showResetFilter = true;
+	}
+	$scope.requestLoading = true;
+	base_off_classes.customGET("", {page: pageNumber, filter: $scope.filterOffClass}).then(function(data) {
+	  if(data.success) {
+	    $scope.off_classes = data.off_classes;
+	    $scope.totalOffClasses = data.count;
+	    $scope.pagination = {current: pageNumber || 1};
+	  }
+	  $scope.requestLoading = false;
+	});
+      };
+      
+      $scope.resetFilter = function() {
+	$scope.filterOffClass ={}
+	$scope.showResetFilter = false;
+	$scope.pagination = {current: 1};
+	getResultsPage(1, true);
+      };
+      
+      $scope.pageChanged = function(newPage, checkFilter) {
+	getResultsPage(newPage, checkFilter);
       };
 
-      load_off_classes();
+      get_filter_data();
+      getResultsPage(1, true);
+
       
     }
     //end of off_classes index
