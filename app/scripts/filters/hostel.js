47a85aa6b06fe493d2d950943b9905fcb84c47f2
SwapnilPatil <swapnil.patil04@gmail.com>
2016-08-18 18:41:46 +0530

    work on hostel student search

---
+++ app/scripts/filters/hostel.js
@@ -0,0 +1,26 @@
+'use strict';
+var app;
+
+app = angular.module('eracordUiApp.filters');
+
+
+app.filter('hostelStudents', function() {
+
+  // Create the return function and set the required parameter name to **input**
+  return function(input, regex) {
+    var patt = new RegExp(regex, 'i');
+    var out = [];
+
+    // Using the angular.forEach method, go through the array of data and perform the operation of figuring out if the language is statically or dynamically typed.
+    angular.forEach(input, function(student) {
+      if (patt.test(student.name) && regex !== '') {
+	student.isHighlight = true;
+      }else {
+	student.isHighlight = false;
+      }
+      out.push(student);
+      
+    });
+    return out;
+  };
+});
