667d748c54dac25e08f3a87a401a308eae670e88
SwapnilPatil <swapnil.patil04@gmail.com>
2016-10-31 09:23:23 +0530

    work on text only directive

--- app/scripts/directives/globel.js
+++ app/scripts/directives/globel.js
@@ -150,3 +150,30 @@ app.directive('onEnterEvent', function() {
     }
   };
 });
+
+
+app.directive('onlyText', function () {
+    return {
+      require: 'ngModel',
+      restrict: 'A',
+      link: function (scope, element, attr, ctrl) {
+        function inputValue(val) {
+          if (val) {
+            var digits = val.replace(/[^A-Za-z_ ]/g, '');
+
+            if (digits.split('.').length > 2) {
+              digits = digits.substring(0, digits.length - 1);
+            }
+
+            if (digits !== val) {
+              ctrl.$setViewValue(digits);
+              ctrl.$render();
+            }
+            return parseFloat(digits);
+          }
+          return undefined;
+        }            
+        ctrl.$parsers.push(inputValue);
+      }
+    };
+ });
