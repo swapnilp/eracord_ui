3ff1530783305246f1944492e647221b535400e1
SwapnilPatil <swapnil.patil04@gmail.com>
2016-12-10 13:17:05 +0530

    work on Link roll base

--- app/scripts/directives/role_base.js
+++ app/scripts/directives/role_base.js
@@ -49,3 +49,51 @@ app.directive('isRoleVisible', function(Restangular) {
     }
   }
 });
+
+
+app.directive('isRoleVisibleLink', function(Restangular) {
+  return {
+    restrict: 'AE',
+    scope: {
+      roles: '=isRoleVisibleLink',
+      email: '=',
+      onlyRoot: "="
+    },
+    controller: ['$scope', '$cookieStore', function(scope, $cookieStore){
+      var roles = [];
+      
+      
+      var getRoles = function(){
+	if ($cookieStore.get('currentUser') !== undefined && $cookieStore.get('currentUser').roles !== undefined) {
+	  roles = $cookieStore.get('currentUser').roles.split(',');
+	}else{
+	  roles = [];
+	}
+      };
+
+      scope.checkRole = function(){
+	getRoles();
+	if(_.contains(roles, "admin")){
+	  if(_.contains(scope.roles, "accountant")){
+	    return(_.intersection(roles, ["accountant"]).length);
+	  }
+	  return 1;
+	}
+	return(_.intersection(roles, scope.roles).length);
+      }
+     }],
+    link: function(scope, element, attrs) {
+      scope.$watch('email', function(){
+	if(scope.checkRole() == 0) {
+	  element.removeAttr('href');
+	} else {
+	  if(scope.onlyRoot === false){
+	    element.removeAttr('href');
+	  }else {
+	    //element.show();
+	  }
+	}
+      });
+    }
+  }
+});
