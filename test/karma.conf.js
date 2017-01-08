17f0055ea009c78e93e8af2820b5a3fc8416f883
SwapnilPatil <swapnil.patil04@gmail.com>
2017-01-09 00:24:26 +0530

    work on UI

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -57,6 +57,7 @@ module.exports = function(config) {
       'bower_components/bootstrap-daterangepicker/daterangepicker.js',
       'bower_components/angular-daterangepicker/js/angular-daterangepicker.js',
       'bower_components/angular-lazy-img/release/angular-lazy-img.js',
+      'bower_components/angular-fileupload/angular-filereader.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
